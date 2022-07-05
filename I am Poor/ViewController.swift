30575272ce6738e280178d8154e59a9032e4a708
Lizandro Franco <lizandrof273@gmail.com>
2022-07-04 21:15:56 -0700

    Added basic xCode outline

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Lizandro Franco on 7/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
